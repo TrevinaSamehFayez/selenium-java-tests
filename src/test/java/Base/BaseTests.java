90e5a4f3a16bf2691a296d5899728f3cddb2a69b
TrevinaSamehFayez <venasameh123@gmail.com>
2025-09-06 15:53:39 +0300

    first commit

--- src/test/java/Base/BaseTests.java
+++ src/test/java/Base/BaseTests.java
@@ -4,6 +4,10 @@ import Pages.HomePage;
 import Utils.WindowManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.openqa.selenium.support.events.WebDriverListener;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -26,9 +30,9 @@ public class BaseTests {
     @BeforeClass
     public void setup(){
         System.setProperty("webdriver.chrome.driver", "Resources/chromedriver.exe");
-
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(getChromeOptions());
         goHome();
+        setCookie();
         driver.manage().window().maximize();
         System.out.println(driver.getTitle());
 
@@ -63,6 +67,19 @@ public class BaseTests {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private ChromeOptions getChromeOptions(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+        return options;
+    }
+
+    private void setCookie(){
+        Cookie cookie = new Cookie.Builder("tau","123").domain("the-internet.herokuapp.com").build();
+        driver.manage().addCookie(cookie);
+
+
     }
 
 
